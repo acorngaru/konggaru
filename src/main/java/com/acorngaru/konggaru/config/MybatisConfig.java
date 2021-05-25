f7e8fc3d2e84f5eeb21a49903e7b5c10febc6f30
oognuyh <oognuyh@gmail.com>
2021-05-25 15:16:36 +0900

    feat: add spring and database settings

---
+++ src/main/java/com/acorngaru/konggaru/config/MybatisConfig.java
@@ -0,0 +1,41 @@
+package com.acorngaru.konggaru.config;
+
+import lombok.AllArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@AllArgsConstructor
+@MapperScan({"com.acorngaru.konggaru.mapper"})
+public class MybatisConfig {
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        //sqlSessionFactoryBean.setMapperLocations(applicationContext.getResource("classpath:/mapper/*.xml"));
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.acorngaru.konggaru.model");
+
+        Properties properties = new Properties();
+        properties.setProperty("mapUnderscoreToCamelCase", "true");
+
+        sqlSessionFactoryBean.setConfigurationProperties(properties);
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
---
+++ src/main/java/com/acorngaru/konggaru/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.acorngaru.konggaru.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(
+        basePackages = "com.acorngaru.konggaru",
+        includeFilters = {
+                @Filter(
+                        type = FilterType.ANNOTATION,
+                        classes = { Controller.class }
+                )
+        }
+)
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    }
+}
