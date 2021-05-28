1dc6b7de637ffc0780e3b2a18e9831acd86f0029
oognuyh <oognuyh@gmail.com>
2021-05-28 17:47:44 +0900

    style: fix typos

--- src/main/java/com/acorngaru/konggaru/repository/S3Repository.java
+++ src/main/java/com/acorngaru/konggaru/repository/S3Repository.java
@@ -3,6 +3,6 @@ package com.acorngaru.konggaru.repository;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Repository {
-    String upload(MultipartFile image) throws Exception;
 
+    String upload(MultipartFile image) throws Exception;
 }
