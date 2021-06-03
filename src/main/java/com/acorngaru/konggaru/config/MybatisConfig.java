e96d0bdc82ecb27fecc0967650162f7fa4085135
oognuyh <oognuyh@gmail.com>
2021-06-03 15:52:43 +0900

    style: uncomment setMapperLocations

--- src/main/java/com/acorngaru/konggaru/config/MybatisConfig.java
+++ src/main/java/com/acorngaru/konggaru/config/MybatisConfig.java
@@ -5,9 +5,9 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -16,19 +16,16 @@ import java.util.Properties;
 @AllArgsConstructor
 @MapperScan({"com.acorngaru.konggaru.mapper"})
 public class MybatisConfig {
-    private final ApplicationContext applicationContext;
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 
         sqlSessionFactoryBean.setDataSource(dataSource);
-        /*
         sqlSessionFactoryBean.setMapperLocations(
                 new PathMatchingResourcePatternResolver()
                         .getResources("classpath:mapper/*.xml")
         );
-        */
         sqlSessionFactoryBean.setTypeAliasesPackage("com.acorngaru.konggaru.model");
 
         Properties properties = new Properties();
